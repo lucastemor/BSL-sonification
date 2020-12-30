2d0441b15628fca10257aaba9cbfd2f872175c01
lucastemor <lucas.temor@mail.utoronto.ca>
2020-12-30 12:12:22 -0500

    setting up i/o paths and directories, testing ignore for output

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BSL-sonification
 Tools for sonification of BSL data, examples, documentation
+
+Currently getting project to working state 
--- add_sound_to_video.py
+++ add_sound_to_video.py
@@ -1,9 +1,46 @@
-"""
-Add sound to video -- input link to video and synthdef and case
+	""" Render sonification and add to video frames or existing video
 
-"""
+	Parameters
+	----------
+	case : str
+		name of Aneurisk case
+	video_path : str
+		path to visualization video. If .mp4 extension, sound will be rendered, scaled, and added to existing video.
+		If no extension, it is assumed to be a path to .png frames. Desired video must be specified and video frames will be rendered
+	video_length : str, conditional
+		Needed if video_path is for a directory of .png frames rather than a file, see video_path for more
+
+	Returns
+	-------
+
+	Notes
+	-----
+
+	"""
+
+import sys
+from pathlib import Path
+
+
 def test():
 	"""
 	This is a test function and it works -- updating the docs 
 	"""
 	return
+
+
+if __name__ == '__main__':
+	
+	#for quick debugging purposes, args are hardcoded if not specified.
+	try:
+		case = sys.argv[1]
+		video_path = sys.argv[2]
+	except:
+		case = 'c0053'
+		video_path = Path('/home/lucas/Documents/viz/renders/Matrix_iterations/aneurisk/c0053added_waveform/waveform_matrix._60fps_1stride.mp4' )
+
+	
+	render_name = video_path.stem
+	outdir = Path('output').joinpath(render_name)
+	if outdir.exists() == False:
+		outdir.mkdir(parents=True)
